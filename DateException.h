041e9baae931b7192bfe4bb10d1f1242dfbadf4d
Omar Ramirez <omarerc@surfpocket.com>
2024-12-20 20:03:08 -0800

    macOS - Clang++ for M2 --

--- DateException.h
+++ DateException.h
@@ -1,19 +1,21 @@
 #ifndef DATEEXCEPTION_H
 #define DATEEXCEPTION_H
 
-#include <iostream>
 #include <exception>
 #include <string>
 
-class DateException: public std::exception {
+class DateException : public std::exception {
 public:
-    explicit DateException(const std::string& message): msg_(message) {}
-    virtual const char* what() const noexcept override {
+    explicit DateException(const std::string& message) : msg_(message) {}
+    virtual ~DateException() _NOEXCEPT { }
+
+    // Removing noexcept specification to rely on default behavior
+    virtual const char* what() const _NOEXCEPT {
         return msg_.c_str();
     }
 
 private:
-    std::string msg_;
+    std::string msg_; // Ensure this member variable is declared correctly
 };
 
-#endif
+#endif // DATEEXCEPTION_H
--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ echo "Building started..."
 # GCC and ToolChain STAGING_DIR path
 #export CXX=/home/omarerc/Documents/projects/wrt/openwrt-sdk-mvebu-cortexa53_gcc-14.2.0_musl.Linux-x86_64/staging_dir/host/bin/g++
 #export STAGING_DIR=/home/omarerc/Documents/projects/wrt/openwrt-sdk-mvebu-cortexa53_gcc-14.2.0_musl.Linux-x86_64/staging_dir/host
-export CXX=/bin/g++
+export CXX=/usr/bin/clang++
 export STAGING_DIR=/
 make
